8d28391771e18e052f7570d2840a5e145c728afd
Ben Schmidt <benjamin.schmidt@ucalgary.ca>
2024-03-24 16:23:02 -0600

    Installed Drizzle and NextUI

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,23 @@
-const express = require('express');
+import express from 'express';
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
 
 const app = express();
 const port = 3001;
+
+
+try {
+    const connection = await mysql.createConnection({
+        host: "host",
+        user: "user",
+        database: "database",
+        // Placeholder, fill this out later
+    });
+
+    const db = drizzle(connection);
+} catch (err) {
+    console.error("Error connecting to the database: ", err);
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
