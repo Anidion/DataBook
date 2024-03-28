6eeed029f4fff753ac1c244f90e2cd8c083a61b2
Ben Schmidt <benjamin.schmidt@ucalgary.ca>
2024-03-27 19:12:40 -0600

    Added full DB schema

--- backend/db/schema.js
+++ backend/db/schema.js
@@ -4,13 +4,16 @@ import {
   serial,
   boolean,
   datetime,
+  bigint,
+  text,
+  primaryKey,
 } from "drizzle-orm/mysql-core";
 
 import { sql } from "drizzle-orm";
 
 // DB Schema
 
-const defaultTimestamps = {
+const timestamps = {
   createdAt: datetime("createdAt").default(sql`CURRENT_TIMESTAMP`),
   updatedAt: datetime("updatedAt").default(
     sql`CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`,
@@ -23,5 +26,143 @@ export const user = mysqlTable("user", {
   password: varchar("password", { length: 255 }),
   email: varchar("email", { length: 255 }),
   isAdmin: boolean("isAdmin"),
-  ...defaultTimestamps,
+  ...timestamps,
 });
+
+export const book = mysqlTable("book", {
+  isbn: varchar("isbn", { length: 13 }).primaryKey(),
+  title: varchar("title", { length: 255 }),
+  quantity: bigint("quantity", { mode: "number", unsigned: true }),
+  publisher: bigint("publisher", {
+    mode: "number",
+    unsigned: true,
+  }).references(() => publisher.id),
+  ...timestamps,
+});
+
+export const author = mysqlTable("author", {
+  id: serial("id"),
+  name: varchar("name", { length: 255 }),
+  bio: text("bio"),
+  ...timestamps,
+});
+
+export const writtenby = mysqlTable(
+  "writtenby",
+  {
+    isbn: varchar("isbn", { length: 13 }).references(() => book.isbn),
+    author: bigint("author", { mode: "number", unsigned: true }).references(
+      () => author.id,
+    ),
+    ...timestamps,
+  },
+  // We need to do this to specify composite primary keys
+  (table) => ({
+    pk: primaryKey({ columns: [table.isbn, table.author] }),
+  }),
+);
+
+export const publisher = mysqlTable("publisher", {
+  id: serial("id"),
+  name: varchar("name", { length: 255 }),
+  yearFounded: bigint("yearFounded", { mode: "number", unsigned: true }),
+  location: varchar("location", { length: 255 }),
+  ...timestamps,
+});
+
+export const genre = mysqlTable("genre", {
+  id: serial("id"),
+  name: varchar("name", { length: 255 }),
+  ...timestamps,
+});
+
+export const bookisgenre = mysqlTable(
+  "bookisgenre",
+  {
+    isbn: varchar("isbn", { length: 13 }).references(() => book.isbn),
+    genre: bigint("genre", { mode: "number", unsigned: true }).references(
+      () => genre.id,
+    ),
+    ...timestamps,
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.isbn, table.genre] }),
+  }),
+);
+
+export const userlikes = mysqlTable(
+  "userlikes",
+  {
+    user: bigint("user", { mode: "number", unsigned: true }).references(
+      () => user.id,
+    ),
+    genre: bigint("genre", { mode: "number", unsigned: true }).references(
+      () => genre.id,
+    ),
+    ...timestamps,
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.user, table.genre] }),
+  }),
+);
+
+export const review = mysqlTable("review", {
+  id: serial("id"),
+  user: bigint("user", { mode: "number", unsigned: true }).references(
+    () => user.id,
+  ),
+  isbn: varchar("isbn", { length: 13 }).references(() => book.isbn),
+  rating: bigint("rating", { mode: "number", unsigned: true }),
+  content: text("content"),
+  ...timestamps,
+});
+
+export const adminapproves = mysqlTable(
+  "adminapproves",
+  {
+    admin: bigint("admin", { mode: "number", unsigned: true }).references(
+      () => user.id,
+    ),
+    review: bigint("review", { mode: "number", unsigned: true }).references(
+      () => review.id,
+    ),
+    approved: boolean("approved"),
+    ...timestamps,
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.admin, table.review] }),
+  }),
+);
+
+export const transaction = mysqlTable(
+  "transaction",
+  {
+    id: serial("id"),
+    user: bigint("user", { mode: "number", unsigned: true }).references(
+      () => user.id,
+    ),
+    isbn: varchar("isbn", { length: 13 }).references(() => book.isbn),
+    startdate: datetime("startdate"),
+    enddate: datetime("enddate"),
+    ...timestamps,
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.id, table.user, table.isbn] }),
+  }),
+);
+
+export const reservation = mysqlTable(
+  "reservation",
+  {
+    id: serial("id"),
+    user: bigint("user", { mode: "number", unsigned: true }).references(
+      () => user.id,
+    ),
+    isbn: varchar("isbn", { length: 13 }).references(() => book.isbn),
+    startdate: datetime("startdate"),
+    ...timestamps,
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.id, table.user, table.isbn] }),
+  }),
+);
